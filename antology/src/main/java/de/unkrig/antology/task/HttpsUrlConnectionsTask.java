cea6d3f44c0e3cd55846ac866f33f6cd4a882e84
Arno Unkrig <aunkrig@users.noreply.github.com>
2018-11-16 14:38:56 +0100

    Added the "<httpsUrlConnections>" task.

---
+++ antology/src/main/java/de/unkrig/antology/task/HttpsUrlConnectionsTask.java
@@ -0,0 +1,67 @@
+
+/*
+ * antology - Some contributions to APACHE ANT
+ *
+ * Copyright (c) 2018, Arno Unkrig
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ *       following disclaimer.
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ *       following disclaimer in the documentation and/or other materials provided with the distribution.
+ *    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package de.unkrig.antology.task;
+
+import java.net.URL;
+import java.security.GeneralSecurityException;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+import de.unkrig.commons.net.security.HttpsUrlConnections;
+
+/**
+ * @see #setDisableSslCertificateValidation(boolean)
+ */
+public
+class HttpsUrlConnectionsTask extends Task  {
+
+    private boolean disableSslCertificateValidation;
+
+    /**
+     * If {@code true}, then SSL certification validation is disabled for all future HTTP {@link URL} connections.
+     */
+    public void
+    setDisableSslCertificateValidation(boolean value) { this.disableSslCertificateValidation = value; }
+
+    @Override public void
+    execute() {
+        try {
+            this.execute2();
+        } catch (Exception e) {
+            throw new BuildException(e);
+        }
+    }
+
+    private void
+    execute2() throws GeneralSecurityException {
+
+        if (this.disableSslCertificateValidation) {
+            HttpsUrlConnections.disableSslCertificateValidation();
+        }
+    }
+}
